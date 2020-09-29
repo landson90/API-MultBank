52d5ff9aabb322a9144f788e509d74da5f97b1f6
Landson <randelsilva1990@gmail.com>
2020-09-29 15:21:02 -0300

    criando endpoint para lista os client.

--- src/main/java/com/example/app/multbanck/multbank/controller/ClientController.java
+++ src/main/java/com/example/app/multbanck/multbank/controller/ClientController.java
@@ -1,10 +1,14 @@
 package com.example.app.multbanck.multbank.controller;
 
+import com.example.app.multbanck.multbank.dto.ClientDTO;
 import com.example.app.multbanck.multbank.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "clientes")
@@ -17,5 +21,8 @@ public class ClientController {
         this.clientService = clientService;
     }
 
-
+    @GetMapping
+    public ResponseEntity<List<ClientDTO>> index() {
+        return this.clientService.index();
+    }
 }
--- src/main/java/com/example/app/multbanck/multbank/service/ClientService.java
+++ src/main/java/com/example/app/multbanck/multbank/service/ClientService.java
@@ -1,8 +1,14 @@
 package com.example.app.multbanck.multbank.service;
 
+import com.example.app.multbanck.multbank.dto.ClientDTO;
+import com.example.app.multbanck.multbank.modal.ClientEntity;
 import com.example.app.multbanck.multbank.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -12,5 +18,14 @@ public class ClientService {
     @Autowired
     public ClientService(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
+    }
+
+
+    public ResponseEntity<List<ClientDTO>> index() {
+        List<ClientEntity> clientEntities = this.clientRepository.findAll();
+        List<ClientDTO> clientDTOS = clientEntities.stream().map(clients -> new ClientDTO(clients))
+                                                            .collect(Collectors.toList());
+
+        return  ResponseEntity.ok().body(clientDTOS);
     }
 }
