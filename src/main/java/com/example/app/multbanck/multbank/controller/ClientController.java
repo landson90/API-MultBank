0967efc2da388f86a6c048bf45ec6f42e33e1334
Landson <randelsilva1990@gmail.com>
2020-09-29 15:31:04 -0300

    criando o endpoint para criar um client.

--- src/main/java/com/example/app/multbanck/multbank/controller/ClientController.java
+++ src/main/java/com/example/app/multbanck/multbank/controller/ClientController.java
@@ -3,10 +3,9 @@ package com.example.app.multbanck.multbank.controller;
 import com.example.app.multbanck.multbank.dto.ClientDTO;
 import com.example.app.multbanck.multbank.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +23,11 @@ public class ClientController {
     @GetMapping
     public ResponseEntity<List<ClientDTO>> index() {
         return this.clientService.index();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<ClientDTO> store(@RequestBody ClientDTO clientDTO) {
+        return this.clientService.store(clientDTO);
     }
 }
--- src/main/java/com/example/app/multbanck/multbank/service/ClientService.java
+++ src/main/java/com/example/app/multbanck/multbank/service/ClientService.java
@@ -4,11 +4,20 @@ import com.example.app.multbanck.multbank.dto.ClientDTO;
 import com.example.app.multbanck.multbank.modal.ClientEntity;
 import com.example.app.multbanck.multbank.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.URI;
 
 @Service
 public class ClientService {
@@ -27,5 +36,24 @@ public class ClientService {
                                                             .collect(Collectors.toList());
 
         return  ResponseEntity.ok().body(clientDTOS);
+    }
+
+
+    public ResponseEntity<ClientDTO> store(ClientDTO clientDTO) {
+        ClientEntity clientEntity = this.convertClientDtoToClientEntity(clientDTO);
+        clientEntity = this.clientRepository.save(clientEntity);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(clientEntity.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new ClientDTO(clientEntity));
+    }
+
+    private ClientEntity convertClientDtoToClientEntity(ClientDTO clientDTO) {
+        return new ClientEntity(
+                clientDTO.getId(),
+                clientDTO.getName(),
+                clientDTO.getEmail(),
+                clientDTO.getPassword());
     }
 }
