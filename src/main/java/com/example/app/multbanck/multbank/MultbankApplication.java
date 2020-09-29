300f249000082e39eec0df68510f1c2a47d84a68
Landson <randelsilva1990@gmail.com>
2020-09-29 14:46:58 -0300

    Primeiro commit do projeto .

---
+++ src/main/java/com/example/app/multbanck/multbank/MultbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.multbanck.multbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app/multbanck/multbank/MultbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.multbanck.multbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
