8c36778598465a8cdbeaf0ec3f0fd8e9931da910
Landson <randelsilva1990@gmail.com>
2020-10-01 10:33:45 -0300

    criando model conta

---
+++ src/main/java/com/example/app/multbanck/multbank/modal/AccountEntity.java
@@ -0,0 +1,73 @@
+package com.example.app.multbanck.multbank.modal;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "contas")
+public class AccountEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero_conta")
+    private String numberAccount;
+
+    @Column(name = "cliente")
+    private ClientEntity clientEntity;
+
+    public AccountEntity() { }
+
+    public AccountEntity(Long id, String numberAccount, ClientEntity clientEntity) {
+        this.id = id;
+        this.numberAccount = numberAccount;
+        this.clientEntity = clientEntity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumberAccount() {
+        return numberAccount;
+    }
+
+    public void setNumberAccount(String numberAccount) {
+        this.numberAccount = numberAccount;
+    }
+
+    public ClientEntity getClientEntity() {
+        return clientEntity;
+    }
+
+    public void setClientEntity(ClientEntity clientEntity) {
+        this.clientEntity = clientEntity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountEntity that = (AccountEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountEntity{" +
+                "id=" + id +
+                ", numberAccount='" + numberAccount + '\'' +
+                ", clientEntity=" + clientEntity +
+                '}';
+    }
+}
