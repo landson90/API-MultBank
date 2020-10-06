8923752f8e3e175ee6f29dc1afa4bfe82d557e0c
Landson <randelsilva1990@gmail.com>
2020-10-06 16:45:46 -0300

    romovendo itens do projeto .

--- src/main/java/com/example/app/multbanck/multbank/dto/PhotoDTO.java
+++ src/main/java/com/example/app/multbanck/multbank/dto/PhotoDTO.java
@@ -1,23 +1,28 @@
 package com.example.app.multbanck.multbank.dto;
 
 import com.example.app.multbanck.multbank.model.ClientEntity;
-import com.example.app.multbanck.multbank.model.PhotoEntity;
 import org.springframework.web.multipart.MultipartFile;
+
+
+import java.util.Objects;
 
 public class PhotoDTO {
 
     private Long id;
+    private String descricao;
     private MultipartFile arquivo;
-    private ClientEntity clientEntity;
 
 
     public PhotoDTO() {
     }
 
-    public PhotoDTO(PhotoEntity photoEntity) {
-        this.id = photoEntity.getId();
-        this.arquivo = photoEntity.getArquivo();
-        this.clientEntity = photoEntity.getClientEntity();
+    public PhotoDTO(Long id,
+                    ClientEntity clientEntity,
+                    String descricao,
+                    MultipartFile arquivo) {
+        this.id = id;
+        this.descricao = descricao;
+        this.arquivo = arquivo;
     }
 
     public Long getId() {
@@ -36,11 +41,26 @@ public class PhotoDTO {
         this.arquivo = arquivo;
     }
 
-    public ClientEntity getClientEntity() {
-        return clientEntity;
+    public String getDescricao() {
+        return descricao;
     }
 
-    public void setClientEntity(ClientEntity clientEntity) {
-        this.clientEntity = clientEntity;
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PhotoDTO photoDTO = (PhotoDTO) o;
+        return id.equals(photoDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
 }
