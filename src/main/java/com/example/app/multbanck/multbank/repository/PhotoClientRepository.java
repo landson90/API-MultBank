40bbcb5a202180d1300b851f164207f6bc8c2924
Landson <randelsilva1990@gmail.com>
2020-10-15 15:09:13 -0300

    fremovendo lfuncionalidade de salvar foto.

--- src/main/java/com/example/app/multbanck/multbank/repository/PhotoClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.multbanck.multbank.repository;
-
-import com.example.app.multbanck.multbank.model.PhotoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhotoClientRepository extends JpaRepository<PhotoEntity, Long> {
-}
