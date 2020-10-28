6a569f5a31fd506693f3851ef6765963dcfbbf2d
Landson <randelsilva1990@gmail.com>
2020-10-28 10:47:53 -0300

    removendo itens

--- src/main/java/com/example/app/multbanck/multbank/config/storage/StorageService.java
+++
@@ -1,8 +0,0 @@
-package com.example.app.multbanck.multbank.config.storage;
-
-import com.example.app.multbanck.multbank.dto.PhotoClientStorageDTO;
-
-public interface StorageService {
-
-    void storageClinetPhoto(PhotoClientStorageDTO photoClientStorageDTO);
-}
--- src/main/java/com/example/app/multbanck/multbank/config/stores/PhotoClientStores.java
+++
@@ -1,8 +0,0 @@
-package com.example.app.multbanck.multbank.config.stores;
-
-import com.example.app.multbanck.multbank.dto.FileStoreDTO;
-
-public interface PhotoClientStores {
-
-    void storesLocal(FileStoreDTO fileStoreDTO);
-}
