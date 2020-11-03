c3bbcbc9480ae5a5fbd172487c052a52e2ec0e29
Landson <randelsilva1990@gmail.com>
2020-11-03 11:35:20 -0300

    CORS heroku

--- src/main/java/com/example/app/multbanck/multbank/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/app/multbanck/multbank/config/security/SecurityConfiguration.java
@@ -48,6 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         config.setAllowedOrigins(Arrays.asList("https://multbank-front-spa-angular.web.app"));
         config.setAllowedOrigins(Arrays.asList("https://multbank-front-spa-angular.firebaseapp.com"));
+        config.setAllowedOrigins(Arrays.asList("https://multbank-front.herokuapp.com/"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         source.registerCorsConfiguration("/**", config);
